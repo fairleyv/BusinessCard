197e0fed9968d7bfaed0b79c9977fe987504d8ec
fairleyv <fairleyvincent@gmail.com>
2024-07-24 11:07:43 -0700

    Provided structure for About and Interests sections and classNames.

--- BusinessCard/components/About.jsx
+++ BusinessCard/components/About.jsx
@@ -1,1 +1,10 @@
 import React from "react";
+
+export default function About () {
+    return (
+    <div className="section">
+        <h4 className="subheading">About</h4>
+        <p className="paragraph">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex obcaecati necessitatibus optio praesentium exercitationem! Similique distinctio asperiores sit debitis illo quaerat assumenda reprehenderit quo tempora suscipit, id exercitationem officiis dolore?</p>
+    </div>
+    )
+}
