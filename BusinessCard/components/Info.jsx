8bf3be906dd1d7876bfeda9b98fc63eb3e1a25bd
fairleyv <fairleyvincent@gmail.com>
2024-07-24 11:34:14 -0700

    added mailto and linkedin link to buttons in info component and created span tags for icons.

--- BusinessCard/components/Info.jsx
+++ BusinessCard/components/Info.jsx
@@ -9,8 +9,14 @@ export default function Info () {
                 <h4 className="job">Job Title</h4>
             </div>
             <div className="infoBtns">
-                <btn className="emailBtn">Email</btn>
-                <btn className="linkedInBtn">Linkedin</btn>
+                <btn className="emailBtn">
+                    <a href="mailto:fairleyvincent@gmail.com">
+                    <span className="emailIcon"></span>Email</a>
+                </btn>
+                <btn className="linkedInBtn">
+                    <a href="www.linkedin.com/in/vincent-fairley">
+                    <span className="linkedInIcon"></span>Linkedin</a>
+                </btn>
             </div>
         </div>
     )
