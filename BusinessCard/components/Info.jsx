4c5226fc6e57318746046cbc97fa3357269c54c3
fairleyv <fairleyvincent@gmail.com>
2024-07-24 11:02:15 -0700

    Finished the structure and classNames for Info.jsx

--- BusinessCard/components/Info.jsx
+++ BusinessCard/components/Info.jsx
@@ -1,1 +1,18 @@
 import React from "react";
+
+export default function Info () {
+    return (
+        <div>
+            <img className="personalPhoto"/>
+            <div className="personalInfo">
+                <h3 className="name">Name</h3>
+                <h4 className="job">Job Title</h4>
+                <p className="website">Name Of Portfolio Site</p>
+            </div>
+            <div className="infoBtns">
+                <btn className="emailBtn">Email</btn>
+                <btn className="linkedInBtn">Linkedin</btn>
+            </div>
+        </div>
+    )
+}
