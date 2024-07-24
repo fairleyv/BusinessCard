5db9a670278949652219e2d41709265f34fe3691
fairleyv <fairleyvincent@gmail.com>
2024-07-23 17:24:33 -0700

    Initial Commit redone with Regular Javascript

---
+++ BusinessCard/src/App.jsx
@@ -0,0 +1,20 @@
+import Info from "../components/Info"
+import About from "../components/About"
+import Interests from "../components/Interests"
+import Footer from "../components/Footer"
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Info />
+      <About />
+      <Interests />
+      <Footer />
+    </>
+  )
+}
+
+export default App
